489c5580470483c97e373e34f38f01bc7e0f663a
old789 <old789@users.noreply.github.com>
2025-06-24 17:33:59 +0300

    improved logging and exceptions handling

--- _send_notification.py
+++ _send_notification.py
@@ -1,10 +1,12 @@
 def send_notification(token, chat, text):
-    url = f'https://api.telegram.org/bot{token}/sendMessage'
-    params = {'chat_id': chat, 'text': text, 'parse_mode': 'HTML'}
-    if config.dry_run:
-      return
-    response = requests.post(url, data=params)
-    if not response.ok:
-      log.error(f'Telegram server return HTTP response code {response.status_code} ("{response.reason}")')
+  url = f'https://api.telegram.org/bot{token}/sendMessage'
+  params = {'chat_id': chat, 'text': text, 'parse_mode': 'HTML'}
+  if config.dry_run:
     return
+  response = requests.post(url, data=params)
+  if response.ok:
+    log.debug(f'Message to telegram sended succesfully')
+  else:
+    log.error(f'Telegram server return HTTP response code {response.status_code} ("{response.reason}")')
+  return
 
--- _update_rrd.py
+++ _update_rrd.py
@@ -3,10 +3,17 @@
 def update_rrd(filename, value, timestamp='N'):
   global rrd_path
   filename = os.path.join(rrd_path, filename + '.rrd')
-  if os.path.exists(filename):
-    if config.dry_run:
-      return
-    rrdtool.update(filename, timestamp+':'+str(float(value)))
-  else:
+  if not os.path.exists(filename):
     log.error(f'File {filename} does not exists')
+    return
+  if config.dry_run:
+    return
+  upd = timestamp+':'+str(float(value))
+  log.debug(f'Updating RRD {filename} with data {upd}')
+  try:
+    rrdtool.update(filename, upd)
+  except ProgrammingError as err:
+    log.error(f'Can\'t update RRD {filename} because programming error "{err}"')
+  except OperationalError as err:
+    log.error(f'Can\'t update RRD {filename} because operational error "{err}"')
 
