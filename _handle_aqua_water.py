428f74893de6590a6ed73d0b46996ed68a8b65c8
old789 <old789@users.noreply.github.com>
2025-06-24 18:16:39 +0300

    some optimization

--- _handle_aqua_water.py
+++ _handle_aqua_water.py
@@ -6,26 +6,32 @@ def handle_aqua_water(msg):
   topic_name = 'aquarium_water_t'
   t = float(msg.payload)
   txt = 'Temperature of water in an aquarium'
+  info_msg = ''
 
-  update_rrd(topic_name, msg.payload)
+  update_rrd(topic_name, t)
   log.debug(f'{txt} {t}{chr(0xB0)}')
 
   if ( t >= threshold_high ):
     if not msg_aqua_threshold_high_sended:
-      send_notification(tlg[tlg_channel]['token'], tlg[tlg_channel]['chat'], f'{txt} is too high ({t}{chr(0xB0)})!')
+      info_msg = f'{txt} is too high ({t}{chr(0xB0)})!'
       msg_aqua_threshold_high_sended = True
   else:
     if msg_aqua_threshold_high_sended:
-      send_notification(tlg[tlg_channel]['token'], tlg[tlg_channel]['chat'], f'{txt} back to normal ({t}{chr(0xB0)})!')
+      info_msg = f'{txt} back to normal ({t}{chr(0xB0)})!'
       msg_aqua_threshold_high_sended = False
 
   if ( threshold_low >= t ):
     if not msg_aqua_threshold_low_sended:
-      send_notification(tlg[tlg_channel]['token'], tlg[tlg_channel]['chat'], f'{txt} is too low ({t}{chr(0xB0)})!')
+      info_msg = f'{txt} is too low ({t}{chr(0xB0)})!'
       msg_aqua_threshold_low_sended = True
   else:
     if msg_aqua_threshold_low_sended:
-      send_notification(tlg[tlg_channel]['token'], tlg[tlg_channel]['chat'], f'{txt} back to normal ({t}{chr(0xB0)})!')
+      info_msg = f'{txt} back to normal ({t}{chr(0xB0)})!'
       msg_aqua_threshold_low_sended = False
+
+  if len(info_msg) > 0:
+      log.info(info_msg)
+      send_notification(tlg[tlg_channel]['token'], tlg[tlg_channel]['chat'], info_msg)
+
   return
 
